ad35d48bcf8503cbb2c869d3bc07d516dea207dd
rpvnwnkl <mattmills@outlook.com>
2013-07-20 13:12:38 -0400

    blog formatting

--- wp-content/themes/roots_base/templates/content.php
+++ wp-content/themes/roots_base/templates/content.php
@@ -2,12 +2,18 @@
   
   <?php if (has_post_thumbnail()) : ?>
 	  <div class="row-fluid">
-	  	<div class="span9">
+	  	<div class="span12">
+	  		<div class="row-fluid">
+	  			<div class="span12">
 			<header>
 			    <a href="<?php the_permalink(); ?>"><h2 class="entry-title"><?php the_title(); ?></h2></a>
+		  </header>
+		</div>
+		</div>
+		<div class="row-fluid">
+		<div class="span9">
 			    <h4 class="entry-cats"><a href="<?php get_category_link(); ?>"><?php the_category('&#8194;/&#8194;'); ?></a></h4>
 			    <?php get_template_part('templates/entry-meta'); ?>
-		  </header>
 		  <div class="entry-summary">
 		    <?php the_excerpt(); ?>
 		  </div>
@@ -17,6 +23,8 @@
 				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 			</div>
 		</div>
+	  </div>
+	  </div>
 	  </div> 
   <?php else : ?>
 	  <header>
--- wp-content/themes/roots_base/templates/head.php
+++ wp-content/themes/roots_base/templates/head.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?> xmlns:fb="http://ogp.me/ns/fb#"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
   
